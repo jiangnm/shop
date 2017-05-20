64cbc304f2f919a04c1e5d8678bd4075911222ec
jiangnm <2306301212@qq.com>
2017-05-20 13:17:28 +0800

    商城

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+	public function index()
+	{
+		return $this->fetch();
+	}
+
+	public function grids()
+	{
+		return $this->fetch();
+	}
+
+	public function media()
+	{
+		return $this->fetch();
+	}
+
+	public function general()
+	{
+		return $this->fetch();
+	}
+
+	public function typography()
+	{
+		return $this->fetch();
+	}
+
+	public function widgets()
+	{
+		return $this->fetch();
+	}
+
+	public function inbox()
+	{
+		return $this->fetch();
+	}
+
+	public function compose()
+	{
+		return $this->fetch();
+	}
+
+	public function tables()
+	{
+		return $this->fetch();
+	}
+
+	public function forms()
+	{
+		return $this->fetch();
+	}
+
+	public function validation()
+	{
+		return $this->fetch();
+	}
+
+	public function login()
+	{
+		return $this->fetch();
+	}
+
+	public function signup()
+	{
+		return $this->fetch();
+	}
+
+	public function blankpage()
+	{
+		return $this->fetch();
+	}
+
+	public function charts()
+	{
+		return $this->fetch();
+	}
+
+}
